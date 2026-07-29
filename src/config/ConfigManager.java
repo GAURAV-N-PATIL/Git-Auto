cddbe52ade6b3d4bf4b1a8bf4382ddfb0be74d38
GAURAV-N-PATIL <gauravnpatil242007@gmail.com>
2026-07-29 20:17:30 +0530

    chore : Configration constants are parsed

--- src/config/ConfigManager.java
+++ src/config/ConfigManager.java
@@ -1,2 +1,20 @@
-
-GitAutoConfig config=manager.load("config/gitauto.properties");
+package config;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+public class ConfigManager{
+	public GitAutoConfig load(String configPath) throws IOException{
+		Properties properties = new Properties();
+		try(FileInputStream fis=new FileInputStream(configPath)){
+			properties.load(fis);
+		}
+		GitAutoConfig config=new GitAutoConfig();
+		config.setWatchPath(properties.getProperty("watch.path"));
+		config.setIdleTime(Integer.parseInt(properties.getProperty("idle.time")));
+		config.setAutoCommit(Boolean.parseBoolean(properties.getProperty("auto.commit")));
+		config.setAutoPush(Boolean.parseBoolean(properties.getProperty("auto.push")));
+		config.setGitBranch(properties.getProperty("git.branch"));
+		config.setLogLevel(properties.getProperty("log.level"));
+		return config;
+	}
+}
